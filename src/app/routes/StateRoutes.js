d006f95c6a9831af6d371ee97c1f12098367f1e7
Guillermo Angulo <anyermo@gmail.com>
2020-04-01 20:24:10 -0300

    state validators added to routes

--- src/app/routes/StateRoutes.js
+++ src/app/routes/StateRoutes.js
@@ -9,9 +9,9 @@ const router = express.Router();
 router.get('/', StateController.index);
 router.get('/id/:stateId', StateValidator.find, StateController.find);
 router.get('/:code', StateValidator.findByCode, StateController.findByCode);
-router.post('/', authMiddleware, StateController.create);
-router.put('/id/:stateId', authMiddleware, StateController.update);
-router.put('/:code', authMiddleware, StateController.updateByCode);
-router.delete('/:stateId', authMiddleware, StateController.remove);
+router.post('/', StateValidator.create, authMiddleware, StateController.create);
+router.put('/id/:stateId', StateValidator.update, authMiddleware, StateController.update);
+router.put('/:code', StateValidator.updateByCode, authMiddleware, StateController.updateByCode);
+router.delete('/:stateId', StateValidator.remove, authMiddleware, StateController.remove);
 
 module.exports = router;
